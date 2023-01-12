64779143c43a4ac3a495e8338d873fd5fe2a21b2
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-01-12 15:42:15 -0500

    added data to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.css';
 import receipts from "./receiptData"
+import {useState} from "react"
 
 function App() {
   const [receipt, setReceipt] = useState(receipts)
-  return (
+  return (<>
     <div className="App">
-
+      <h1>Korilla Receipts</h1>
+      {receipts.map((receipt) => {
+        return (
+          <div className="receipt">
+        <h2>{receipt.person}</h2>
+        <h3>{receipt.order.main}</h3>
+        <article key={receipt.id}>
+          <div>Protein: {receipt.order.protein}</div>
+          <div>Rice: {receipt.order.rice}</div>
+          <div>Sauce: {receipt.order.sauce}</div>
+          <div>Drink: {receipt.order.drink}</div>
+          <div>Cost: {receipt.order.cost}</div>
+          <div>Paid: </div>
+        </article>
+        </div>
+        )
+      })}
     </div>
+    </>
   );
 }
 
--- src/components/Index.js
+++
@@ -1,5 +0,0 @@
-function Index (props) {
-    return <h1>This is the Index Component</h1>
-}
-
-export default Index
