c4e2539936e5b6c791c7a23694380cc7d6498e00
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-01-12 15:29:05 -0500

    initial commit

---
+++ src/components/Index.js
@@ -0,0 +1,5 @@
+function Index (props) {
+    return <h1>This is the Index Component</h1>
+}
+
+export default Index
---
+++ src/receiptData.js
@@ -0,0 +1,43 @@
+const receipts = [
+   {
+    id:1,
+    person: "Karolin",
+    order: {
+      main: "Burrito",
+      protein: "Organic Tofu",
+      rice: "Purple Rice",
+      sauce: "Green Crack",
+      drink: "Korchata",
+      cost: 22
+    },
+    paid: false
+  },
+   {
+    id:2,
+    person: "Jerrica",
+    order: {
+      main: "Rice Bowl",
+      protein: "Ginger Soy Chix",
+      rice: "Sticky Rice",
+      sauce: "Korilla",
+      drink: "Korchata",
+      cost: 19
+    },
+    paid: false
+  },
+   {
+    id:3,
+    person: "Matt",
+    order: {
+      main: "Salad Bowl",
+      protein: "Organic Tofu",
+      rice: "none",
+      sauce: "K'lla",
+      drink: "Sparkling Blood Orange Soda",
+      cost: 20
+    },
+    paid: false
+  }
+]
+
+export default receipts
